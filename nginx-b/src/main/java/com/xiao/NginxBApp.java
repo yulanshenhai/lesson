11f0523c30645384cabb5ab6ba7f7f815bcc8e4e
yulanshenhai <3360851236@qq.com>
2023-01-05 17:56:46 +0800

    lesson项目

---
+++ nginx-b/src/main/java/com/xiao/NginxBApp.java
@@ -0,0 +1,14 @@
+package com.xiao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xiao
+ */
+@SpringBootApplication
+public class NginxBApp {
+    public static void main(String[] args) {
+        SpringApplication.run(NginxBApp.class, args);
+    }
+}
---
+++ nginx-b/src/main/java/com/xiao/controller/NginxController.java
@@ -0,0 +1,16 @@
+package com.xiao.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author xiao
+ */
+@RestController
+public class NginxController {
+
+    @GetMapping("execute")
+    public String execute(){
+        return "我是nginx-b项目";
+    }
+}
