93bfcfdef8bc7a27b970563a44481f31859b149d
yulanshenhai <3360851236@qq.com>
2023-01-05 18:10:29 +0800

    lesson项目

--- nginx-a/src/main/java/com/xiao/controller/NginxController.java
+++ nginx-a/src/main/java/com/xiao/controller/NginxController.java
@@ -3,14 +3,21 @@ package com.xiao.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * @author xiao
  */
 @RestController
 public class NginxController {
 
-    @GetMapping("execute")
-    public String execute(){
+    @GetMapping("hello")
+    public String hello(){
         return "我是nginx-a项目";
+    }
+
+    @GetMapping("get-session")
+    public String getSession(HttpServletRequest req){
+        return "nginx-a项目的session-id是：" + req.getSession().getId();
     }
 }
--- nginx-b/src/main/java/com/xiao/controller/NginxController.java
+++ nginx-b/src/main/java/com/xiao/controller/NginxController.java
@@ -3,14 +3,21 @@ package com.xiao.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * @author xiao
  */
 @RestController
 public class NginxController {
 
-    @GetMapping("execute")
-    public String execute(){
+    @GetMapping("hello")
+    public String hello(){
         return "我是nginx-b项目";
+    }
+
+    @GetMapping("get-session")
+    public String getSession(HttpServletRequest req){
+        return "nginx-b项目的session-id是：" + req.getSession().getId();
     }
 }
